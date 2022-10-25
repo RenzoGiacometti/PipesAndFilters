b673d86a8ed72c283791a3ef4786dddc4b1021f8
Renzo Giacometti <renzo.giacometti@gmail.com>
2022-10-25 08:36:22 -0300

    .

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,20 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"beer.jpg");
+            
+            PipeNull pipeNull = new PipeNull();
+            FilterGreyscale filterGreyscale = new FilterGreyscale();
+            FilterNegative filterNegative = new FilterNegative();
+            FilterSave filterSaveIntermedio = new FilterSave("ImagenIntermedia.jpg");
+            FilterSave filterSave = new FilterSave("BeerB&W.jpg");
+            PipeSerial pipeSerialGuardado2 = new PipeSerial(filterSave, pipeNull);
+            PipeSerial pipeSerial1 = new PipeSerial(filterNegative, pipeSerialGuardado2);
+            PipeSerial pipeSerialGuardado = new PipeSerial(filterSaveIntermedio, pipeSerial1);
+            PipeSerial pipeSerial2 = new PipeSerial(filterGreyscale, pipeSerialGuardado);
+            
+            pipeSerial2.Send(picture);
         }
     }
 }
